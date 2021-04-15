4457b6c6019dc18d9580e530ff5525a54167fff8
lishujun <lishujun@youedata.com>
2021-04-15 14:17:39 +0800

    handleGeoloaction

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <button @click="handleGeoloaction">获取位置</button>
+    <p>{{msg}}</p>
+  </div>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+  },
+  data() {
+    return {
+      msg: 'hello'
+    }
+  },
+  methods: {
+    handleGeoloaction() {
+      if(navigator.geolocation.getCurrentPosition) {
+        navigator.geolocation.getCurrentPosition(this.getPositionSucess, this.getPositionError)
+      } else {
+        throw new Error('您的浏览器不支持获取用户位置')
+      }
+    },
+    getPositionSucess(position) {
+      console.log('sucess', position)
+      console.log(position.timestamp); // 1525364883361
+      console.log(position.coords);     // Coordinates {...}
+    },
+    getPositionError(e) {
+      console.log('error', e)
+      console.log(e.code)
+      console.log(e.e.message)
+    }
   }
 }
 </script>
